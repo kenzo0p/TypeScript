78bc2608e16019d70ad0e912e0bc1a488628aabd
Om Bhor <ombhor617@gmail.com>
2024-10-29 14:36:55 +0530

    type alias

--- 02Basics/myObjects.js
+++ 02Basics/myObjects.js
@@ -1,16 +1,11 @@
 "use strict";
+// const User = {
+//     name:"om",
+//     email:"o@o.com",
+//     isActive:true
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-var User = {
-    name: "om",
-    email: "o@o.com",
-    isActive: true
-};
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
+function createUser(user) {
+    return { name: "", email: "", isActive: true };
 }
-var newUser = { name: "om", isPaid: true, email: "o@o.com" };
-// createUser({name:"om" ,isPaid:true , email:"o@o.com"})//it gives an error beacuse email is not there
-createUser(newUser); //but it didn't throw an error ,we can pass more values in it
-function createCourse() {
-    return { name: "typeScript", price: 333 };
-}
+createUser({ name: "", email: "", isActive: true });
--- 02Basics/myObjects.ts
+++ 02Basics/myObjects.ts
@@ -1,24 +1,40 @@
-const User = {
-    name:"om",
-    email:"o@o.com",
-    isActive:true
+// const User = {
+//     name:"om",
+//     email:"o@o.com",
+//     isActive:true
+// }
+
+
+
+
+
+// function createUser({name:string , isPaid:boolean}){}
+
+// let newUser = {name:"om" ,isPaid:true , email:"o@o.com"}
+
+// // createUser({name:"om" ,isPaid:true , email:"o@o.com"})//it gives an error beacuse email is not there
+// createUser(newUser)//but it didn't throw an error ,we can pass more values in it
+
+// function createCourse():{name:string , price:number}{
+//     return {name:"typeScript" ,price:333}
+// }
+
+
+
+
+//type alias
+type User = {
+    name:string;
+    email:string;
+    isActive:boolean
+
 }
 
 
+type myString  =string;//we can assign any to dat types by using type
 
-
-
-function createUser({name:string , isPaid:boolean}){}
-
-let newUser = {name:"om" ,isPaid:true , email:"o@o.com"}
-
-// createUser({name:"om" ,isPaid:true , email:"o@o.com"})//it gives an error beacuse email is not there
-createUser(newUser)//but it didn't throw an error ,we can pass more values in it
-
-function createCourse():{name:string , price:number}{
-    return {name:"typeScript" ,price:333}
+function createUser(user:User):User{
+    return {name:"" , email:"", isActive:true}
 }
-
-
-
+createUser({name:"" , email:"", isActive:true});
 export {}
