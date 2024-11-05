439647d658294507c48355230bed6515f99baf03
Om Bhor <ombhor617@gmail.com>
2024-11-05 14:58:36 +0530

    inheritance

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,10 +1,63 @@
 "use strict";
+// class User {
+//     email: String
+//     name: String
+//     private readonly city: String = ""
+//     constructor( email: String, name: String, city: String) {
+//         this.email = email;
+//         this.name = name;
+//         this.city = city;
+//     }
+// }
+// const om = new User("om@om.com", "om", "pune")
+// class User {
+//     // email: String
+//     // name: String
+//     private readonly city: String = ""
+//     constructor( public email: String,public name: String, city: String) {
+//         this.email = email;
+//         this.name = name;
+//         this.city = city;
+//     }
+// }
+// const om = new User("om@om.com", "om", "pune")
 class User {
-    constructor(email, name) {
+    constructor(email, name, city) {
+        this.email = email;
+        this.name = name;
+        // email: String
+        // name: String
+        this._courseCount = 1;
         this.city = "";
         this.email = email;
         this.name = name;
+        this.city = city;
+    }
+    deleteToken() {
+        console.log("Token Deleted");
+    }
+    get getApplEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNumber) {
+        5;
+        if (courseNumber <= 1) {
+            throw new Error("Course count should be more than one");
+        }
+        this._courseCount = courseNumber;
     }
 }
-const om = new User("om@om.com", "om");
-om.city = "pune";
+const om = new User("om@om.com", "om", "pune");
+// om.deleteToken()
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,12 +1,66 @@
+// class User {
+//     email: String
+//     name: String
+//     private readonly city: String = ""
+//     constructor( email: String, name: String, city: String) {
+//         this.email = email;
+//         this.name = name;
+//         this.city = city;
+//     }
+// }
+
+// const om = new User("om@om.com", "om", "pune")
+// class User {
+//     // email: String
+//     // name: String
+//     private readonly city: String = ""
+//     constructor( public email: String,public name: String, city: String) {
+//         this.email = email;
+//         this.name = name;
+//         this.city = city;
+//     }
+// }
+
+// const om = new User("om@om.com", "om", "pune")
 class User {
-    email:String
-    name:String
-    city:String =""
-    constructor(email :String , name:String  ){
+    // email: String
+    // name: String
+
+    protected _courseCount = 1
+
+    private readonly city: String = ""
+    constructor(public email: String, public name: String, city: String) {
         this.email = email;
         this.name = name;
+        this.city = city;
+    }
+    private deleteToken() {
+        console.log("Token Deleted")
+    }
+    get getApplEmail(): string {
+        return `apple${this.email}`
+    }
+    get courseCount(): number {
+        return this._courseCount
+    }
+    set courseCount(courseNumber) {
+        5
+        if (courseNumber <= 1) {
+            throw new Error("Course count should be more than one")
+        }
+        this._courseCount = courseNumber
     }
 }
 
-const om = new User ("om@om.com" , "om")
-om.city = "pune"
+const om = new User("om@om.com", "om", "pune")
+
+// om.deleteToken()
+
+
+class SubUser extends User {
+    isFamily:boolean =true
+
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
