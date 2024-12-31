038d99be8f311f0d528fd5b71de24a761b718dc4
om bhor <ombhor617@gmail.com>
2024-12-31 13:49:56 +0530

    resume

---
+++ l1/app.ts
@@ -0,0 +1,8 @@
+function add(num1:number ,num2:number){
+
+    return num1+ num2 //only number type will be accepted
+}
+
+console.log(add(1,2))
+
+// +num1 + num2 it will automatically converts the string into the number (+)
