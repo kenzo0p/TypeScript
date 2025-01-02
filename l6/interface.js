7a00a1699084e77d8f2e3a6c3de6364c098684f7
om bhor <ombhor617@gmail.com>
2025-01-02 15:18:23 +0530

    Interface in typescript

--- l6/interface.js
+++ l6/interface.js
@@ -1,3 +1,11 @@
 "use strict";
 // interface 
-console.log("interface hii");
+// console.log("interface hii ")
+const user = {
+    name: "om",
+    age: 20,
+    sayHi() {
+        console.log(`hii ${this.name}`);
+    }
+};
+console.log(user.sayHi());
--- l6/interface.ts
+++ l6/interface.ts
@@ -1,2 +1,18 @@
 // interface 
-console.log("interface hii")
+// console.log("interface hii ")
+
+interface User {
+    name: string;
+    age: number;
+    sayHi(): void;
+}
+
+const user : User ={
+    name:"om",
+    age:20,
+    sayHi() {
+     console.log(`hii ${this.name}`)   
+    }
+}
+
+console.log(user.sayHi())
